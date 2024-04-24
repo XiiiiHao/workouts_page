5a4fc9884fa3150d1535cbbde629d5f4da46dae9
XiiiiHao <haojunxi0106@gmail.com>
2024-04-24 12:15:58 +0800

    Update logo in site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -13,7 +13,7 @@ interface ISiteMetadataResult {
 const data: ISiteMetadataResult = {
   siteTitle: 'Workouts Map',
   siteUrl: 'https://github.com/XiiiiHao',
-  logo: 'https://raw.githubusercontent.com/XiiiiHao/workouts_page/master/public/images/d7bc08ac4724d36f95cc40d621b6351b.jpg',
+  logo: 'https://raw.githubusercontent.com/XiiiiHao/workouts_page/master/public/images/b_e6b1aa644ecce50aaff07d759047ea25.jpg',
   description: 'Personal site and blog',
   keywords: 'workouts, running, cycling, riding, roadtrip, hiking, swimming',
   navLinks: [
